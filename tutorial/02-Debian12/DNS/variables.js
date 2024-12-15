25465be4c9b4b3366258904a0e849b3d91cc2952
Binar Nugroho <binarnugroho775@gmail.com>
2024-12-15 10:12:35 +0700

    tutorial: fix variables import

--- tutorial/02-Debian12/DNS/variables.js
+++ tutorial/02-Debian12/DNS/variables.js
@@ -1,15 +1,15 @@
-export const SERVER_1_HOST = "211.27.65.10";
-export const SERVER_2_HOST = "211.27.65.20";
-export const CLIENT_HOST = "173.23.0.2";
-export const SERVER_1_HOST_REVERSE = SERVER_1_HOST.split(".").reverse().join(".");
-export const SERVER_1_NAME = "LNXSRV1";
-export const SERVER_2_NAME = "LNXSRV2";
-export const CLIENT_NAME = "client-1";
-export const FQDN = "lkslab.net";
-export const BIND_CONF_PREFIX_PATH = "/etc/bind";
-export const BIND_F_ZONE_FILE_PATH = `${BIND_CONF_PREFIX_PATH}/db.${FQDN}`;
-export const BIND_R_ZONE_FILE_PATH = `${BIND_CONF_PREFIX_PATH}/db.27.211`;
-export const BIND_NAMED_CONF_PATH = `${BIND_CONF_PREFIX_PATH}/named.conf.local`;
+const SERVER_1_HOST = "211.27.65.10";
+const SERVER_2_HOST = "211.27.65.20";
+const CLIENT_HOST = "173.23.0.2";
+const SERVER_1_HOST_REVERSE = SERVER_1_HOST.split(".").reverse().join(".");
+const SERVER_1_NAME = "LNXSRV1";
+const SERVER_2_NAME = "LNXSRV2";
+const CLIENT_NAME = "client-1";
+const FQDN = "lkslab.net";
+const BIND_CONF_PREFIX_PATH = "/etc/bind";
+const BIND_F_ZONE_FILE_PATH = `${BIND_CONF_PREFIX_PATH}/db.${FQDN}`;
+const BIND_R_ZONE_FILE_PATH = `${BIND_CONF_PREFIX_PATH}/db.27.211`;
+const BIND_NAMED_CONF_PATH = `${BIND_CONF_PREFIX_PATH}/named.conf.local`;
 
 export default {
   SERVER_1_HOST,
