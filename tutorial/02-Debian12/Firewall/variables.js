8961a7861c48364582ab76629a02ad577fbc461f
Binar Nugroho <binarnugroho775@gmail.com>
2024-12-19 22:36:11 +0700

    feat: add iptables firewall configuration tutorial for Debian

---
+++ tutorial/02-Debian12/Firewall/variables.js
@@ -0,0 +1,23 @@
+const HOST_CIDR = "/32";
+const CLIENT_CIDR = "/26";
+const SERVER_CIDR = "/18";
+const SERVER_1_ADDR = "211.27.65.10";
+const SERVER_1_OS = "Linux Debian";
+const CLIENT_1_OS = "Windows 10";
+const CLIENT_1_ADDR = "173.23.0.19";
+const SERVER_1_NAME = "LNXSRV1";
+const CLIENT_1_NAME = "CLIENT1";
+const FQDN = "lkslab.net";
+
+export default {
+  HOST_CIDR,
+  CLIENT_CIDR,
+  SERVER_CIDR,
+  SERVER_1_ADDR,
+  SERVER_1_OS,
+  CLIENT_1_OS,
+  CLIENT_1_ADDR,
+  SERVER_1_NAME,
+  CLIENT_1_NAME,
+  FQDN,
+};
