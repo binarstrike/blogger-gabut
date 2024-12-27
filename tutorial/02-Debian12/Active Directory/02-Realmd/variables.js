eb496dbad887000f7d716da2493856964b5671c2
Binar Nugroho <binarnugroho775@gmail.com>
2024-12-28 01:26:19 +0700

    feat: add tutorial for installing and configuring realmd on Debian 12

---
+++ tutorial/02-Debian12/Active Directory/02-Realmd/variables.js
@@ -0,0 +1,20 @@
+// domain controller
+const DC_1_ROOT_DOMAIN = "lkslab.com";
+const DC_1_HOST = "211.27.89.1";
+const DC_1_HOSTNAME = "dc01";
+const DC_1_OS = "Windows Server 2022";
+
+// server machine
+const SERVER_1_HOST = "173.23.0.68";
+const SERVER_1_HOSTNAME = "lnx-srv-1";
+const SERVER_1_OS = "Debian 12";
+
+export default {
+  DC_1_ROOT_DOMAIN,
+  DC_1_HOST,
+  DC_1_HOSTNAME,
+  SERVER_1_HOST,
+  SERVER_1_HOSTNAME,
+  DC_1_OS,
+  SERVER_1_OS,
+};
